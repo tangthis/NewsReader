7463bb8fc4b7bfd1a136dcfff9b5c64a3917369d
Jerry <tangthis@163.com>
2015-10-22 22:32:54 +0800

    新闻页下拉加载更多

---
+++ NewsReader/ViewController/Cell/NewsMoreCell.h
@@ -0,0 +1,13 @@
+//
+//  NewsMoreCell.h
+//  NewsReader
+//
+//  Created by 唐有欢 on 15/10/22.
+//  Copyright © 2015年 tangtech. All rights reserved.
+//
+
+#import "BaseCell.h"
+
+@interface NewsMoreCell : BaseCell
+
+@end
---
+++ NewsReader/ViewController/Cell/NewsMoreCell.m
@@ -0,0 +1,18 @@
+//
+//  NewsMoreCell.m
+//  NewsReader
+//
+//  Created by 唐有欢 on 15/10/22.
+//  Copyright © 2015年 tangtech. All rights reserved.
+//
+
+#import "NewsMoreCell.h"
+
+@implementation NewsMoreCell
+
+-(void)initCell
+{
+    [super initCell];
+}
+
+@end
