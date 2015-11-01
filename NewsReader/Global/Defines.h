cc618f43d5becd8323bf5177441353187dd8d6b5
Jerry <tangthis@163.com>
2015-11-01 20:14:41 +0800

    “服务器端程序”

--- NewsReader/Global/Defines.h
+++ NewsReader/Global/Defines.h
@@ -8,15 +8,15 @@
 
 
 #if (DEBUG || TESTCASE)
-#define FxLog(format, ...) NSLog(format, ## __VA_ARGS__)
+#define Log(format, ...) NSLog(format, ## __VA_ARGS__)
 #else
-#define FxLog(format, ...)
+#define Log(format, ...)
 #endif
 
 // 日志输出宏
-#define BASE_LOG(cls,sel) FxLog(@"%@-%@",NSStringFromClass(cls), NSStringFromSelector(sel))
-#define BASE_ERROR_LOG(cls,sel,error) FxLog(@"ERROR:%@-%@-%@",NSStringFromClass(cls), NSStringFromSelector(sel), error)
-#define BASE_INFO_LOG(cls,sel,info) FxLog(@"INFO:%@-%@-%@",NSStringFromClass(cls), NSStringFromSelector(sel), info)
+#define BASE_LOG(cls,sel) Log(@"%@-%@",NSStringFromClass(cls), NSStringFromSelector(sel))
+#define BASE_ERROR_LOG(cls,sel,error) Log(@"ERROR:%@-%@-%@",NSStringFromClass(cls), NSStringFromSelector(sel), error)
+#define BASE_INFO_LOG(cls,sel,info) Log(@"INFO:%@-%@-%@",NSStringFromClass(cls), NSStringFromSelector(sel), info)
 
 // 日志输出函数
 #if (DEBUG || TESTCASE)
